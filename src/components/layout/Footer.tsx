034061707ebbed74008a0d4543439c1d7506850e
Ersin Çelik <clk.ersinnn@gmail.com>
2024-12-30 22:39:21 +0300

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,13 +1,1 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-gray-600">
-      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-        <p className="text-center text-sm">
-          © 2024 TeknoKapsül. All rights reserved.
-        </p>
-      </div>
-    </footer>
-  );
-}
+ 
