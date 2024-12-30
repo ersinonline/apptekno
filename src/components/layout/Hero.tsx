691bd1fb77417322d5beb57933c3d3d7635fa552
Ersin Çelik <clk.ersinnn@gmail.com>
2024-12-30 22:37:14 +0300

    Update Hero.tsx

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -5,7 +5,7 @@ import { slides } from '../../utils/constants';
 export default function Hero() {
   return (
     <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3 sm:py-4">
-      <div className="aspect-[16/7] rounded-xl overflow-hidden shadow-xl">
+      <div className="aspect-[16/6,5] rounded-xl overflow-hidden shadow-xl">
         <Slider slides={slides} />
       </div>
     </div>
